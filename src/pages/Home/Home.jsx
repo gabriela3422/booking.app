21e843e31fcae81bccf0b2b7b125de2b80b95d00
gabriela3422 <trifugabrielaemilia30@yahoo.com>
2024-04-23 20:50:14 +0300

    update project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,54 @@
 import "./Home.scss";
+import axios, {options} from "axios";
+import {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBed, faRoute, faCarSide, faPersonSnowboarding, faShip, faPlane} from "@fortawesome/free-solid-svg-icons";
+
 const Homepage = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await axios('http://localhost:8000/categories');
+                setCategories(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchCategories();
+    }, []);
+
+    const categoryIcons = {
+        'Hotel': faBed,
+        'Tour': faRoute,
+        'Activity': faPersonSnowboarding,
+        'Car': faCarSide,
+        'Cruise': faShip,
+        "Flights": faPlane
+    };
+
+    const getCategoryIcon = (categoryName) => {
+        return categoryIcons[categoryName] || null;
+    };
+
+
     return (
         <>
-            <section id="main-page">
-
+            <section id="masthead">
+                <div className="container mx-auto">
+                    <div className="masthead-tabs">
+                        <div className="tabs-controls d-flex items-center js-tabs-controls">
+                            {categories.map(category => (
+                            <button key={category.id}
+                                className="tabs-button px-7 py-5 rounded-4 fw-600 text-white js-tabs-button is-tab-el-active">
+                                <FontAwesomeIcon icon={getCategoryIcon(category.name)}/>
+                                {category.name}
+                            </button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
