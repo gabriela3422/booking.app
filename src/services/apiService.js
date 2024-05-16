0e534c750e6fb1be4327068b437b3c0b8634e45d
gabriela3422 <trifugabrielaemilia30@yahoo.com>
2024-05-16 18:41:38 +0300

    added env to api server

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+console.log(apiUrl);
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: `${apiUrl}`,
 });
 
 const apiService = {
